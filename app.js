9033454fd7b1b0934816872f1a9254145141026b
Brian Gunawan <brgunawan@uk-c02z24f1lvdr.uk.deloitte.com>
2019-10-03 16:01:07 +0100

    try

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
