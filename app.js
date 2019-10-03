379aab6d013e84ceff217bdda2ab44401e008d27
Brian Gunawan <brgunawan@uk-c02z24f1lvdr.uk.deloitte.com>
2019-10-03 15:53:30 +0100

    try

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 3000
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+});
 
-
-var express = require('express');
-var app = express();
-var port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port,() => {
+    console.log(`Server running at port `+port);
+});
